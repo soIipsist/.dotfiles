17429faffa804a2f2e93025ed8c0bbd22c3a0f2c
soIipsist <solipsist14@gmail.com>
2024-12-02 14:04:41 +0200

    .tmux.conf

--- mac/.tmux/tmux.sh
+++ mac/.tmux/tmux.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-source "../dotfiles.sh"
-
-dir="$PWD/.tmux"
-dotfiles=$(get_dotfiles $dir)
-destination_directory="$HOME"
-
-move_dotfiles "${dotfiles[@]}" "${destination_directory}"
+# custom configuration for later
--- mac/.zsh/zsh.sh
+++ mac/.zsh/zsh.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 source "../dotfiles.sh"
 
-dir="$PWD/.zsh"
-dotfiles=$(get_dotfiles $dir)
-destination_directory="$HOME"
-
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-move_dotfiles "${dotfiles[@]}" "${destination_directory}"
