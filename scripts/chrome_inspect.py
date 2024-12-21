485eb15d41f572444c37b723c9b7e604f09182c5
soIipsist <solipsist14@gmail.com>
2024-12-21 23:12:41 +0200

    chrome inspect

--- scripts/chrome_inspect.py
+++ scripts/chrome_inspect.py
@@ -1,13 +1,43 @@
 # inspect currently open google chrome tab
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeService
+import subprocess
 
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+# open chrome in debugging mode
+cmd = [
+    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    "--remote-debugging-port=9222",
+    "--user-data-dir=/tmp/chrome-debug",
+]
+chrome_profile_path = ""
+cmd = r'xcopy "%LOCALAPPDATA%\Google\Chrome\User Data\Default" C:\TempProfile /E /I /H /C /K /O /X'
+
+# Execute the command
+try:
+    result = subprocess.run(
+        cmd,
+        shell=True,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    print(result.stdout)
+except subprocess.CalledProcessError as e:
+    print("Error occurred:")
+    print(e.stderr)
 
 chromedriver_path = "/opt/homebrew/bin/chromedriver"
-driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+service = ChromeService(chromedriver_path)
 
-# Fetch the HTML of the current tab
+driver = webdriver.Chrome(options=chrome_options, service=service)
+driver.get("https://www.google.com")
+# Get the HTML content of the current page
 html = driver.execute_script("return document.documentElement.outerHTML;")
 print(html)
+
+# Close the browser
+driver.quit()
