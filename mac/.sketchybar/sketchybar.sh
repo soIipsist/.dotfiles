c987c7e0467f4b71aedcf2da04c65ee1e4abafe1
soIipsist <solipsist14@gmail.com>
2025-01-28 10:27:49 +0200

    sketchybar - plugins

--- mac/.sketchybar/sketchybar.sh
+++ mac/.sketchybar/sketchybar.sh
@@ -1,2 +1,5 @@
 destination_directory="$HOME/.config/sketchybar"
+cp -f "aerospace.sh" "$destination_directory/plugins/aerospace.sh"
+cp -f "aerospace.sh" "$destination_directory/plugins/aerospace.sh"
+
 echo $destination_directory
