ad1bc00835d47ec322509137c82c1b84d1af6259
soIipsist <solipsist14@gmail.com>
2025-01-31 15:46:52 +0200

    fonts list

--- mac/.sketchybar/sketchybar.sh
+++ mac/.sketchybar/sketchybar.sh
@@ -3,15 +3,11 @@ plugins_directory="$destination_directory/plugins"
 source_plugins_directory="$PWD/.sketchybar/plugins"
 
 # copy plugins
-cp -f "$source_plugins_directory/aerospace.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/battery.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/calendar.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/clock.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/cpu.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/front_app.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/icon_map_fn.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/media.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/space_windows.sh" "$plugins_directory"
-cp -f "$source_plugins_directory/space.sh" "$plugins_directory"
+files=("aerospace.sh" "battery.sh" "calendar.sh" "clock.sh" "cpu.sh" "front_app.sh" "icon_map_fn.sh" "media.sh" "space_windows.sh" "space.sh")
+
+for file in "${files[@]}"; do
+    cp -f "$source_plugins_directory/$file" "$plugins_directory"
+    chmod +x "$plugins_directory/$file"
+done
 
 echo $destination_directory
