b9d557cc1bb8e4e257154828bc564d1488bd93b4
soIipsist <solipsist14@gmail.com>
2025-02-07 23:34:26 +0200

    sketchybar - removed scripts

--- mac/.sketchybar/plugins/calendar.sh
+++ mac/.sketchybar/plugins/calendar.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
+# The $NAME variable is passed from sketchybar and holds the name of
+# the item invoking this script:
+# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date +'%a %d %b %I:%M %p')"
+sketchybar --set "$NAME" label="$(date '+%a %d %b %H:%M')"
