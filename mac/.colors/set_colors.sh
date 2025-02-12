f66b322f4a3fc3cdb1b043c19fa235c52ac70c07
soIipsist <solipsist14@gmail.com>
2025-02-12 22:15:18 +0200

    set color schemes

--- mac/.colors/set_colors.sh
+++ mac/.colors/set_colors.sh
@@ -11,7 +11,7 @@ fi
 export_file="$destination_directory/colors.sh"
 
 echo "#!/bin/bash" >"$export_file"
-jq -r 'to_entries | .[] | "export \(.key)=\(.value)"' "$color_scheme_path" >>"$export_file"
+jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' "$color_scheme_path" >>"$export_file"
 
 # Load into current shell session
 source "$export_file"
