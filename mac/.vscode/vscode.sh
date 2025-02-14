4aef84c885fec4907df611dfa05ce28df2015247
soIipsist <solipsist14@gmail.com>
2025-02-14 23:02:44 +0200

    set color schemes

--- mac/.vscode/vscode.sh
+++ mac/.vscode/vscode.sh
@@ -1,26 +1,19 @@
 source "../json.sh"
-
 destination_directory="$dotfiles_directory/.config/vscode"
 
-install_extensions() {
-    if [ -z "$1" ]; then
-        echo "VSCode extensions not defined."
-        return
-    fi
-
-}
-
-source_directory="$PWD/.vscode"
+source_directory="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 extensions_path="$source_directory/extensions.json"
 settings_path="$source_directory/settings.json"
-default_vs_code_path="$HOME/Library/Application Support/Code/User"
+default_vs_code_path="$HOME/Library/Application Support/Code/User/settings.json"
 
 extensions=$(get_json_value "recommendations" $extensions_path "")
 
 for extension in $extensions; do
-    bash code --install-extension $extension --force
+    code --install-extension "$extension" --force >&2
 done
 
+# replace colors with environment variables
+colors_path="$dotfiles_directory/.config/colors/colors.sh"
+source "$colors_path"
+envsubst <"$settings_path" >"$default_vs_code_path"
 echo $destination_directory
-
-# cp -f "$settings_path" "$default_vs_code_path"
