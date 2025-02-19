8356e0b62d8f186b9a8f66b43e54a1488a19c15f
soIipsist <solipsist14@gmail.com>
2025-02-19 17:43:22 +0200

    sketchybar - set color scheme

--- mac/.colors/colors.sh
+++ mac/.colors/colors.sh
@@ -1,11 +1,10 @@
 if [ -z $dotfiles_directory ]; then
-    dotfiles_directory=$HOME
+    dotfiles_directory="$HOME"
 fi
 
 destination_directory="$dotfiles_directory/.config/colors"
 source_directory="$PWD/.colors"
 
-mkdir -p $destination_directory
-cp -f "$source_directory/set_colors.sh" "$destination_directory"
+mkdir -p "$destination_directory"
+cp -f "$source_directory/scripts/set_colors.sh" "$destination_directory"
 chmod +x "$destination_directory/set_colors.sh"
-source "$destination_directory/set_colors.sh"
--- mac/.colors/scripts/set_colors.sh
+++ mac/.colors/scripts/set_colors.sh
@@ -1,15 +1,14 @@
 # sets default color scheme based on $color_scheme provided
 
-destination_directory="$dotfiles_directory/.config/colors"
-
 if [ -z "$dotfiles_directory" ]; then
-    dotfiles_directory=$HOME
+    dotfiles_directory="$HOME"
 fi
 
 if [ -z "$color_scheme" ]; then
     color_scheme="colors_1"
 fi
 
+destination_directory="$dotfiles_directory/.config/colors"
 color_scheme_path="$destination_directory/$color_scheme.json"
 exported_colors="$destination_directory/colors.sh"
 
@@ -22,7 +21,10 @@ source "$exported_colors"
 # copy vscode settings path
 vscode_source_path="$dotfiles_directory/.config/vscode/vscode_settings.json"
 vscode_destination_path="$HOME/Library/Application Support/Code/User/settings.json"
-envsubst <"$vscode_source_path" >"$vscode_destination_path"
+
+if [ -f $vscode_source_path ]; then
+    envsubst <"$vscode_source_path" >"$vscode_destination_path"
+fi
 
 # set iterm2 color profile
 # source "$dotfiles_directory/.config/iterm2/iterm2.sh"
