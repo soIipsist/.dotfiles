db520edd42a2cec570aa3f8889e6b0a1a389cce5
soIipsist <solipsist14@gmail.com>
2025-03-05 13:01:58 +0200

    .sketchybar - set template

--- mac/test.sh
+++ mac/test.sh
@@ -4,10 +4,10 @@ source "../dotfiles.sh"
 source "../git.sh"
 source "../json.sh"
 
-SKETCHYBAR_TEMPLATE=$(get_json_value "SKETCHYBAR_TEMPLATE" "$GIT_DOTFILES_DIRECTORY/mac/.colors/main.json")
+SKETCHYBAR_TEMPLATE=($(get_json_value "SKETCHYBAR_TEMPLATE" "$GIT_DOTFILES_DIRECTORY/mac/.colors/main.json"))
 export COPY_PLUGINS=0
 
-# source "./.sketchybar/sketchybar.sh" "$SKETCHYBAR_TEMPLATE"
+source "./.sketchybar/sketchybar.sh" "$SKETCHYBAR_TEMPLATE"
 
 # source "./.sketchybar/templates/set_template.sh" "${templates[@]}"
-echo_line_to_file "hello world" "./.sketchybar/templates/xrce_bottom" 2
+# echo_line_to_file "hello world" "./.sketchybar/templates/xrce_bottom" 2
