6d4887ff677072e96c0db78182d0ff9c04ef2470
soIipsist <solipsist14@gmail.com>
2025-03-09 10:55:54 +0200

    .zsh

--- mac/.zsh/zsh.sh
+++ mac/.zsh/zsh.sh
@@ -1,16 +1,16 @@
 destination_directory="$dotfiles_directory"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-source_scripts_directory="$PWD/.zsh/scripts"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source_scripts_directory="$SCRIPT_DIR/scripts"
 dest_scripts_directory="/usr/local/bin"
 
 # copy scripts to /bin
 bin_scripts=($(ls $source_scripts_directory))
 
-for bin_script in "$bin_scripts"; do
-    if [ -f "$bin_script" ]; then
-        sudo cp -f "$bin_script" "$scripts_directory"
-        sudo chmod +x "$dest_scripts_directory/$(basename "$bin_script")"
-    fi
+for bin_script in "${bin_scripts[@]}"; do
+    sudo cp -f "$source_scripts_directory/$bin_script" "$dest_scripts_directory"
+    sudo chmod +x "$dest_scripts_directory/$(basename "$bin_script")"
+    echo "Copied $bin_script to $dest_scripts_directory."
 done
