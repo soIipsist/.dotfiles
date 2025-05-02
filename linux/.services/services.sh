119b941ff3924ee9e175b3c88fceca8a99179a47
soIipsist <solipsist14@gmail.com>
2025-05-02 12:32:33 +0300

    downloader service

--- linux/.services/services.sh
+++ linux/.services/services.sh
@@ -4,7 +4,7 @@ dest_scripts_directory="/etc//systemd/system/"
 
 services=($(ls $SCRIPT_DIR))
 
-for bin_script in "${bin_scripts[@]}"; do
+for bin_script in "${services[@]}"; do
     sudo cp -f "$source_scripts_directory/$bin_script" "$dest_scripts_directory"
     sudo chmod +x "$dest_scripts_directory/$(basename "$bin_script")"
     echo "Copied $bin_script to $dest_scripts_directory."
