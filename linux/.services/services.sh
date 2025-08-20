a7bde67f8e04ca0d79896a9b5e123c4bddf4ca7c
soIipsist <solipsist14@gmail.com>
2025-08-20 13:21:41 +0300

    downloader service - modified service permissions

--- linux/.services/services.sh
+++ linux/.services/services.sh
@@ -5,8 +5,7 @@ source_services_directory="$SCRIPT_DIR/services"
 dest_services_directory="/etc/systemd/system/"
 dest_config_directory="/etc/default/"
 
-group="shared_group"
-sudo addgroup $group
+UID=$(id -u)
 
 copy() {
     file="$1"
@@ -18,10 +17,6 @@ copy() {
     fi
 
     sudo cp -f "$file" "$dest_directory/$filename"
-
-    # chmod files
-    sudo chmod 664 "$dest_directory/$filename"
-    sudo chown $(whoami):$group "$dest_directory/$filename"
     echo "Copied $filename to $dest_directory."
 }
 
@@ -31,19 +26,16 @@ for file in "$source_services_directory"/*.service; do
     service_name="$(basename "$file")"
     service_base="${service_name%.service}"
 
-    log_dir="srv/shared/tmp"
+    log_dir="/tmp/${UID}/${service_base}"
     log_file="$log_dir/${service_base}.log"
     err_file="$log_dir/${service_base}.err"
 
-    # Create log directory if it doesn't exist
     mkdir -p "$log_dir"
-    sudo chown root:$group "$log_dir"
-    sudo chmod 2775 "$log_dir"
+    chmod 700 "$log_dir"
 
-    # Recreate log files with correct ownership and permissions
-    sudo touch "$log_file" "$err_file"
-    sudo chown $(whoami):$group "$log_file" "$err_file"
-    sudo chmod 664 "$log_file" "$err_file"
+    touch "$log_file" "$err_file"
+    chown $UID:$UID "$log_file" "$err_file"
+    chmod 600 "$log_file" "$err_file"
 
     echo "Created log files for $service_base in $log_dir."
 done
@@ -52,6 +44,7 @@ for file in "$source_services_directory"/*.conf; do
 
     filename="$(basename "$file")"
     temp_file="$(mktemp)"
+    export UID
     envsubst <"$file" >"$temp_file"
 
     state_file=$(grep -oP 'STATE_FILE="\K[^"]+' "$temp_file")
@@ -59,11 +52,8 @@ for file in "$source_services_directory"/*.conf; do
     if [ -n "$state_file" ]; then
         sudo mkdir -p "$(dirname "$state_file")"
         sudo touch "$state_file"
-
         echo "Created state file $state_file"
     fi
-    sudo chown "$(whoami):$group" "$state_file"
-    sudo chmod 664 "$state_file"
 
     copy "$temp_file" "$dest_config_directory" "$filename"
     rm "$temp_file"
