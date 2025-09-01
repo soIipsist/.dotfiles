cf0470251ab97e0b1ecd977dfd42c38177580b59
soIipsist <solipsist14@gmail.com>
2025-09-01 15:08:11 +0300

    ytdlp_channel.py - download

--- scripts/ytdlp_channel.py
+++ scripts/ytdlp_channel.py
@@ -1,28 +1,53 @@
-from ytdlp import get_channel_info
+from ytdlp import get_channel_info, download
 from argparse import ArgumentParser
 from pprint import PrettyPrinter
 
-from downloader import Downloader
+from downloader import Downloader, Download
 
-downloaders = Downloader().select_all()
 pp = PrettyPrinter(indent=2)
-pp.pprint(downloaders)
 
 
-def download(channel_id: str, downloader: str = "ytdlp"):
+def download(
+    channel_id: str,
+    downloader: str = "ytdlp",
+    sleep_interval: str = None,
+    max_sleep_interval: str = None,
+):
+
+    if downloader is None:
+        downloader = Downloader("ytdlp")
+
+    downloader = Downloader(downloader_type=downloader).select_first()
+
+    if not downloader:
+        raise ValueError(f"Downloader of type {downloader} was not found.")
+
+    downloader: Downloader
+
     results = get_channel_info(channel_id)
     video_urls = [
         f"https://www.youtube.com/watch?v={entry['id']}" for entry in results["entries"]
     ]
 
-    for url in video_urls:
-        print(url)
+    downloads = [
+        Download(
+            url=video_url,
+            downloader=downloader,
+            extra_args=f"sleep_interval={sleep_interval}, max_sleep_interval={max_sleep_interval}",
+        )
+        for video_url in video_urls
+    ]
+    downloader.start_downloads(downloads)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("channel_id", type=str)
-    parser.add_argument("-d", "--downloader", choices=[], default="ytdlp")
+    parser.add_argument("-d", "--downloader", default="ytdlp")
+    parser.add_argument("-i", "--sleep_interval", default=None)
+    parser.add_argument("-m", "--max_sleep_interval", default=None)
 
     args = parser.parse_args()
-    download(args.channel_id)
+    download(
+        args.channel_id, args.downloader, args.sleep_interval, args.max_sleep_interval
+    )
