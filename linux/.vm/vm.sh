cf4425cfc7f0186f0235d5fbb992b28e281f1893
soIipsist <solipsist14@gmail.com>
2025-11-16 05:22:05 +0200

    .vm - vm.sh

--- linux/.vm/vm.sh
+++ linux/.vm/vm.sh
@@ -1,4 +1,4 @@
-destination_directory="$HOME/isos"
+destination_directory=${VM_DIR:-"$HOME/isos"}
 source_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$destination_directory"
 cp -rf "$source_dir/config/"* "$destination_directory"
