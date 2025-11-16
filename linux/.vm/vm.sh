5fb3651dc7c69ea3125a82c90ef9eaa0056ba815
soIipsist <solipsist14@gmail.com>
2025-11-16 13:16:56 +0200

    .vm - vm.sh

--- linux/.vm/vm.sh
+++ linux/.vm/vm.sh
@@ -1,18 +1,18 @@
-VM_DIR=${VM_DIR:-"$HOME/isos"}
+VM_DIR=${VM_DIR:-"/var/lib/libvirt/images"}
 source_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$VM_DIR"
 cp -rf "$source_dir/config/"* "$VM_DIR"
 
-echo
-echo "Fix permissions so QEMU can read your ISOs?"
-echo "(This adds you to libvirt-qemu group and fixes $VM_DIR) (y/N)"
-read -r answer
+# echo
+# echo "Fix permissions so QEMU can read your ISOs?"
+# echo "(This adds you to libvirt-qemu group and fixes $VM_DIR) (y/N)"
+# read -r answer
 
-if [[ "$answer" =~ ^[Yy]$ ]]; then
-    sudo usermod -aG libvirt-qemu "$USER"
-    sudo chgrp -R libvirt-qemu "$VM_DIR"
-    sudo chmod -R g+rwX "$VM_DIR"
-    echo "Permissions fixed. (Log out/in for group change to apply)"
-else
-    echo "Skipped permission fix."
-fi
+# if [[ "$answer" =~ ^[Yy]$ ]]; then
+#     sudo usermod -aG libvirt-qemu "$USER"
+#     sudo chgrp -R libvirt-qemu "$VM_DIR"
+#     sudo chmod -R g+rwX "$VM_DIR"
+#     echo "Permissions fixed. (Log out/in for group change to apply)"
+# else
+#     echo "Skipped permission fix."
+# fi
