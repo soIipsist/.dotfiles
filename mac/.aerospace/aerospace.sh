7d29f7c53c4db016d156aca098a10079efacc850
soIipsist <solipsist14@gmail.com>
2026-05-18 12:36:09 -0700

    .aerospace

--- mac/.aerospace/aerospace.sh
+++ mac/.aerospace/aerospace.sh
@@ -1,3 +1,14 @@
+install_aerospace(){
+    if command -v aerospace &>/dev/null; then
+        return 0
+    fi
+
+    brew install --cask nikitabobko/tap/aerospace
+
+    echo "Successfully installed aerospace."
+}
+
+install_aerospace
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$dotfiles_directory/.config/themes/theme.sh"
