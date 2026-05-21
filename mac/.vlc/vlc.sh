e30839ecf5ed69b1106eab913360b8a65bae4f74
soIipsist <solipsist14@gmail.com>
2026-05-21 13:21:32 +0300

    .vlc

--- mac/.vlc/vlc.sh
+++ mac/.vlc/vlc.sh
@@ -1,1 +1,9 @@
+install_vlc(){
+    if [[ -e /Applications/VLC.app ]]; then
+        return
+    fi
+    brew install --cask vlc
+}
+
+install_vlc
 destination_directory="$dotfiles_directory/Library/Preferences/org.videolan.vlc/"
