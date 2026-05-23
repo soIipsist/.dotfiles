f8f0d8705cf47fe504d5308bb7bb8c5eeec79523
soIipsist <solipsist14@gmail.com>
2026-05-23 05:06:14 +0300

    .ytdlp

---
+++ mac/.ytdlp/ytdlp.sh
@@ -0,0 +1,14 @@
+install_ytdlp(){
+    YTDLP_PATH="$HOME/ytdlp/yt-dlp_macos"
+
+    if [ ! -e "$YTDLP_PATH" ]; then
+        echo "Cloning yt-dlp..."
+        mkdir -p "$(dirname "$YTDLP_PATH")"
+        release_name=$(basename $YTDLP_PATH)
+        curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/$release_name -o "$YTDLP_PATH"
+        chmod a+rx "$YTDLP_PATH"
+    fi
+}
+
+
+install_ytdlp
