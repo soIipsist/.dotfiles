cc56fdd5370d7ca1716c934fbb1fa2b8c0670995
soIipsist <solipsist14@gmail.com>
2026-06-05 10:47:52 +0300

    .zsh_aliases fix

--- linux/.wsl/wsl.sh
+++ linux/.wsl/wsl.sh
@@ -1,5 +1,6 @@
 if [[ -n "$WSL_DISTRO_NAME" ]]; then
     echo "Copying in WSL..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 else
     skip_dotfiles=true
     return 0
