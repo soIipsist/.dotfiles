86a5368723589dd62ce3148cfb38c32d8e55d721
soIipsist <solipsist14@gmail.com>
2026-06-17 13:49:34 +0300

    .docker

--- mac/.docker/docker.sh
+++ mac/.docker/docker.sh
@@ -1,25 +1,11 @@
-#!/bin/bash
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONTAINERS_FOLDER="$HOME/.containers"
-COMPOSE_DIR="$SCRIPT_DIR/compose"
+destination_directory="$HOME/.containers"
+CONTAINERS_DIR="$SCRIPT_DIR/.containers"
 
-# Ensure the containers base directory exists
-mkdir -p "$CONTAINERS_FOLDER"
+mkdir -p "$destination_directory"
+cp -R "$CONTAINERS_DIR"/. "$destination_directory"/
 
-compose_files=("$COMPOSE_DIR"/*.{yml,yaml})
-
-for file in "${compose_files[@]}"; do
-    filename=$(basename "$file")
-    service_name="${filename%.*}" 
-
-    dest_dir="$CONTAINERS_FOLDER/$service_name/"
-    final_path="$dest_dir/compose.yaml"
-
-    mkdir -p "$dest_dir"
-
-    echo "Copying '$filename' into directory structure..."
-    cp -f "$file" "$final_path"
-    echo "Successfully copied to: $final_path"
+find "$destination_directory" \( -name "*.yaml" -o -name "*.yml" \) -type f | while read -r file; do
+    dir=$(dirname "$file")
+    mv "$file" "$dir/compose.yaml"
 done
-
